ff0fac86f0f8d0a966a27b946b2445e468909937
Soruj Mahmud <villagecode868@gmai.com>
2026-07-29 14:47:04 +0600

    fix: add remote pattern for res.cloudinary.com in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
         pathname: '/**',
       },
