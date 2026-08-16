95b443da949a8ac3ab13d3a9ad10d0fd22dcbe00
Soruj Mahmud <villagecode868@gmai.com>
2026-08-16 22:02:49 +0600

    feat: update Next.js config to enhance image handling and security policies

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,9 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 }
 
